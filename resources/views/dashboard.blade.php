e301f5ce2999efbc8298885fe13b833e7f530c35
Fikri Wahidin <fikriayam4@gmail.com>
2024-04-16 18:23:24 +0700

    auth update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,8 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+                    <br> <br>
+                    <a href="/note" class="mt-3 mb-2 text-white bg-sky-600 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Note</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ToDoListController;
+use App\Http\Controllers\PasswordController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,45 +17,46 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Note
-Route::get('/note', function () {
-    return view('note');
-});
 
-// ToDoList
-Route::get('/todolist', [ToDoListController::class, 'index']);
-Route::post('/todolist', [ToDoListController::class, 'store']);
-
-// Timer
-Route::get('/timer', function () {
-    return view('timer');
-});
-
-// Reminder
-Route::get('/reminder', function () {
-    return view('reminder');
-});
-Route::get('/add-reminder', function () {
-    return view('add_reminder');
-});
-
-// Password Manager
-Route::get('/passwords', [PasswordController::class, 'index'])->name('passwords.index');
-Route::get('/passwords/create', [PasswordController::class, 'create'])->name('passwords.create');
-Route::post('/passwords', [PasswordController::class, 'store'])->name('passwords.store');
-Route::get('/passwords/{id}/edit', [PasswordController::class, 'edit'])->name('passwords.edit');
-Route::put('/passwords/{id}', [PasswordController::class, 'update'])->name('passwords.update');
-Route::delete('/passwords/{id}', [PasswordController::class, 'destroy'])->name('passwords.destroy');
-
-// Music Player
-Route::get('/musicplayer', function () {
-    return view('musicplayer');
-});
 
 Route::middleware('auth')->group(function () {
+    // Note
+    Route::get('/note', function () {
+        return view('note');
+    });
+
+    // ToDoList
+    Route::get('/todolist', [ToDoListController::class, 'index']);
+    Route::post('/todolist', [ToDoListController::class, 'store']);
+
+    // Timer
+    Route::get('/timer', function () {
+        return view('timer');
+    });
+
+    // Reminder
+    Route::get('/reminder', function () {
+        return view('reminder');
+    });
+    Route::get('/add-reminder', function () {
+        return view('add_reminder');
+    });
+
+    // Password Manager
+    Route::get('/passwords', [PasswordController::class, 'index'])->name('passwords.index');
+    Route::get('/passwords/create', [PasswordController::class, 'create'])->name('passwords.create');
+    Route::post('/passwords', [PasswordController::class, 'store'])->name('passwords.store');
+    Route::get('/passwords/{id}/edit', [PasswordController::class, 'edit'])->name('passwords.edit');
+    Route::put('/passwords/{id}', [PasswordController::class, 'update'])->name('passwords.update');
+    Route::delete('/passwords/{id}', [PasswordController::class, 'destroy'])->name('passwords.destroy');
+
+    // Music Player
+    Route::get('/musicplayer', function () {
+        return view('musicplayer');
+    });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
