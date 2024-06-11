7ddd4ffcb37521b8e0cf84a07caaca470caa36c7
rashadizza <54693288+rashadizza@users.noreply.github.com>
2024-06-11 22:14:44 +0700

    first music

---
+++ app/Models/Song.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    protected $fillable = ['video_id', 'title', 'singer'];
+}
+
